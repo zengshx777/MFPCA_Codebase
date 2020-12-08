b41d1c6ea9dd40c17bd77dce14f2efedc5236543
zengshx777 <34327225+zengshx777@users.noreply.github.com>
2020-12-08 18:35:19 -0500

    updated aoas

--- Simulation/Simu_job.sh
+++ Simulation/Simu_job.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-R CMD BATCH Simulation_main.R &
+R CMD BATCH --vanilla '--args sparsity.level=15 sample.size=200' Simulation_main.R R1.out&
+R CMD BATCH --vanilla '--args sparsity.level=25 sample.size=200' Simulation_main.R R2.out&
+R CMD BATCH --vanilla '--args sparsity.level=50 sample.size=200' Simulation_main.R R3.out&
+R CMD BATCH --vanilla '--args sparsity.level=100 sample.size=200' Simulation_main.R R4.out&
+R CMD BATCH --vanilla '--args sparsity.level=15 sample.size=500' Simulation_main.R R5.out&
+R CMD BATCH --vanilla '--args sparsity.level=25 sample.size=500' Simulation_main.R R6.out&
+R CMD BATCH --vanilla '--args sparsity.level=50 sample.size=500' Simulation_main.R R7.out&
+R CMD BATCH --vanilla '--args sparsity.level=100 sample.size=500' Simulation_main.R R8.out&
+R CMD BATCH --vanilla '--args sparsity.level=15 sample.size=1000' Simulation_main.R R9.out&
+R CMD BATCH --vanilla '--args sparsity.level=25 sample.size=1000' Simulation_main.R R10.out&
+R CMD BATCH --vanilla '--args sparsity.level=50 sample.size=1000' Simulation_main.R R11.out&
+R CMD BATCH --vanilla '--args sparsity.level=100 sample.size=1000' Simulation_main.R R12.out&
--- Simulation/Simulation_main.R
+++ Simulation/Simulation_main.R
@@ -1,9 +1,21 @@
+rm(list=ls())
+
+
+args=commandArgs(trailingOnly = TRUE)
+if(length(args)==0){
+  print("No arguments supplied.")
+}else{
+  for(i in 1:length(args)){
+    eval(parse(text=args[[i]]))
+  }
+}
+
 #Illustration
 source("Simulation_Function.R")
-#Assumption 1,2,3 Valid
-Simu_Wrap=Simulation_Data(nsample_m_mean=15,
-                          nsample_y_mean=15,
-                          nsample_size=200,
+# Simulated dataset
+Simu_Wrap=Simulation_Data(nsample_m_mean=sparsity.level,
+                          nsample_y_mean=sparsity.level,
+                          nsample_size=sample.size,
                           same_grids=T,prob_treated=0.5,
                           #Noise for Underlying Process
                           noise_m=1,
@@ -17,8 +29,7 @@ M_data=Simu_Wrap$M_data
 Y_data=Simu_Wrap$Y_data
 source("Simu_Mediator.R")
 source("Simu_Sampling_Outcome.R")
-save(pos_sample_size, direct_process,indirect_process,
-     total_effect,gamma,file="S1.RData")
+
 
 true_direct_effect_process = mean(y_1_1-y_0_1)
 true_indirect_effect_process = mean(y_0_1-y_0_0)
@@ -29,17 +40,16 @@ indirect_effect_process=apply(simplify2array(indirect_process),1:2,mean)
 total_effect_process=apply(simplify2array(total_effect),1:2,mean)
 gamma_est = mean(gamma)
 
-(mean(indirect_effect_process)-mean(true_indirect_effect_process))
 
-## RANDOM
+## RANDOM EFFECT MODEL
 library(mgcv)
-model_1=gam(Mediator~X1+X2+X3+Treatment+s(Time)+s(ID,bs="re") ,data=M_data)
+m_model_re=gam(Mediator~X1+X2+X3+Treatment+s(Time)+s(ID,bs="re") ,data=M_data)
 Y_data$Mediator = M_data$Mediator
-model_2=gam(Outcome~X1+X2+X3+Mediator+Treatment+s(Time)+s(ID,bs="re") ,data=Y_data)
+y_model_re=gam(Outcome~X1+X2+X3+Mediator+Treatment+s(Time)+s(ID,bs="re") ,data=Y_data)
 
 
 
-## GEE
+## GEE MODEL
 library(gee)
 gee_model_1=gee(Mediator~X1+X2+X3+Treatment,id=ID,data=M_data,corstr = "AR-M",Mv=1)
 gee_model_2=gee(Outcome~X1+X2+X3+Treatment+Mediator,id=ID,data=Y_data,corstr = "AR-M",Mv=1)
@@ -47,4 +57,11 @@ gamma_gee=gee_model_2$coefficients[length(gee_model_2$coefficients)]
 effect_m_gee=gee_model_1$coefficients[length(gee_model_1$coefficients)]
 indirect_effect_gee=gamma_gee*effect_m_gee
 total_gee=indirect_effect_gee+gee_model_2$coefficients[length(gee_model_2$coefficients)-1] 
-
+# Save results
+save(pos_sample_size, direct_process,indirect_process,
+     true_direct_effect_process,true_indirect_effect_process,
+     true_total_effect_process,gamma_est,
+     m_model_re,y_model_re,
+     total_gee,effect_m_gee,gamma_gee,total_gee,
+     total_effect,gamma,gamma_true,
+     file=paste(sparsity.level,sample.size,"_simu_result.RData",sep="_"))
